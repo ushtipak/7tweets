5faf09e1aabcd3b97ee1c7a2a0d408b0a69893b6
Milos Knezevic <ushtipak@gmail.com>
2017-04-25 22:12:21 +0200

    Come on travis, start, start, damn it :)

--- test_seven_tweets.py
+++ test_seven_tweets.py
@@ -8,3 +8,9 @@ test_client = app.test_client()
 def test_get_tweets():
     response = test_client.get('/tweets')
     assert response.get_data(as_text=True) == "[]\n"
+
+
+def test_delete_tweet():
+    response = test_client.get("/tweets/100")
+    assert response.get_data(as_text=True) == "\"no tweet #100\"\n"
+
