1bf8c50a75b5bd5d68e176724c986acb59ee464e
Milos Knezevic <ushtipak@gmail.com>
2017-05-07 21:55:15 +0200

    Fix tests :)

--- test_seven_tweets.py
+++ test_seven_tweets.py
@@ -7,10 +7,10 @@ test_client = app.test_client()
 
 def test_get_tweets():
     response = test_client.get('/tweets')
-    assert response.get_data(as_text=True) == "[]\n"
+    assert response.get_data(as_text=True) == "There are no tweets :("
 
 
 def test_delete_tweet():
     response = test_client.get("/tweets/100")
-    assert response.get_data(as_text=True) == "\"no tweet #100\"\n"
+    assert response.get_data(as_text=True) == "There is no tweet with id 100"
 
