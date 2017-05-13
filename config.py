c39426888fc205ea29b037463d08fbb9898b4528
Milos Knezevic <m@ike>
2017-05-13 13:41:31 +0200

    Add ENV config parameters :)

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
+import os
+
+db_host = os.getenv("ST_DB", "st_db")
+db_user = os.getenv("ST_USER", "st_user")
+db_pass = os.getenv("ST_PASS", "st_pass")
+db_port = 5432
+docker_network = "st_network"
 docker_repository = "ushtipak/7tweets"
 app_name = docker_repository.split("/")[1]
 app_port = 2500
 app_server = "professor-chaos"
-docker_network = "st_network"
 postgres_version = "9.6.2"
 postgres_storage = "/opt/postgres-data"
-db_host = "st_db"
-db_port = 5432
-db_user = "st_user"
-db_name = "tweets"
-db_pass = "st_pass"
 
 DB_CONFIG = dict(user=db_user,
                  host=db_host,
